518e4ed070d84938f2d3c14c6cdc99b287d4724e
anjor <anjor@umd.edu>
2024-09-18 14:57:30 +0100

    add docker host

--- benchmark/docker.sh
+++ benchmark/docker.sh
@@ -8,5 +8,6 @@ docker run \
        -e HISTFILE=/aider/.bash_history \
        -e AIDER_DOCKER=1 \
        -e AIDER_BENCHMARK_DIR=/benchmarks \
+       --network="host" \
        aider-benchmark \
        bash
